43735ad68a1f8cb146ce70ba5bfc14e3bf764917
ssfanist <senior.dev32@gmail.com>
2021-11-16 12:31:32 +1000

    update abis

--- src/pix.ts
+++ src/pix.ts
@@ -1,38 +1,29 @@
 import { BigInt } from "@graphprotocol/graph-ts";
-import { Transfer, PIX as PIXContract } from "./entities/PIX/PIX";
+import { Transfer, PIXMinted } from "./entities/PIX/PIX";
 import { Global, Account, PIX, PIXTransfer } from "./entities/schema";
 import { createAccount } from "./account";
 
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
+export function handlePIXMinted(event: PIXMinted): void {
+  if (event.params.tokenId.isZero()) return;
+
+  createAccount(event.params.account);
+  let pix = new PIX(getPIXId(event.params.tokenId));
+  pix.account = event.params.account.toHexString();
+  pix.tokenId = event.params.tokenId;
+  pix.pixId = event.params.pixId;
+  pix.category = BigInt.fromI32(event.params.category);
+  pix.size = BigInt.fromI32(event.params.size);
+  pix.classification = BigInt.fromI32(event.params.classification);
+  pix.country = event.params.country;
+  pix.save();
+}
+
 export function handleTransfer(event: Transfer): void {
   if (event.params.tokenId.isZero()) return;
 
-  let entity = Global.load("totalSupply");
-  if (entity == null) {
-    entity = new Global("totalSupply");
-    entity.value = new BigInt(0);
-  }
-  if (event.params.to.toHexString() == ZERO_ADDRESS) {
-    entity.value = entity.value.minus(BigInt.fromI32(1));
-  } else if (event.params.from.toHexString() == ZERO_ADDRESS) {
-    entity.value = entity.value.plus(BigInt.fromI32(1));
-  }
-  entity.save();
-
-  if (event.params.from.toHexString() == ZERO_ADDRESS) {
-    let contract = PIXContract.bind(event.address);
-    let pixInfoResult = contract.pixInfos(event.params.tokenId);
-    let pix = new PIX(getPIXId(event.params.tokenId));
-    pix.tokenId = event.params.tokenId;
-    createAccount(event.params.from);
-    createAccount(event.params.to);
-    pix.account = event.params.to.toHexString();
-    pix.pixId = pixInfoResult.value0;
-    pix.category = BigInt.fromI32(pixInfoResult.value1);
-    pix.size = BigInt.fromI32(pixInfoResult.value2);
-    pix.save();
-  } else {
+  if (event.params.from.toHexString() != ZERO_ADDRESS) {
     let pix = PIX.load(getPIXId(event.params.tokenId));
     createAccount(event.params.to);
     pix.account = event.params.to.toHexString();
